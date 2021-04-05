53af8a948680b964fbd35c0d5cff6df5bc974a56
jha2913 <jha2913@naver.com>
2021-04-05 12:21:53 +0900

    04050332

--- src/api/lost.js
+++ src/api/lost.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 export default {
 
-  get: (id) => axios.get(`${process.env.VUE_APP_API_BASE}/lostAndFounds/${id}`),
+  get: (id) => axios.get(`${process.env.VUE_APP_API_BASE}/losts/${id}`),
 
-  patch: (id, final3) => axios.patch(`${process.env.VUE_APP_API_BASE}/lostAndFounds/${id}`, final3),
+  patch: (id, final3) => axios.patch(`${process.env.VUE_APP_API_BASE}/losts/${id}`, final3),
 
-  list: (page) => axios.get(`${process.env.VUE_APP_API_BASE}/lostAndFounds/paging?page=${page}`)
+  list: (page) => axios.get(`${process.env.VUE_APP_API_BASE}/losts/paging?page=${page}`)
 }
